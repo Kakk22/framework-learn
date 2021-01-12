f7f8d26786bad5bbc3f41693f1da0f0a66a2625a
Kakk22 <1029839308@qq.com>
2021-01-12 21:55:00 +0800

    init jvm project

---
+++ jvm/src/main/java/com/cyf/HeadTest.java
@@ -0,0 +1,18 @@
+package com.cyf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 陈一锋
+ * @date 2021/1/11 23:13
+ **/
+public class HeadTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        List<HeadTest> list = new ArrayList<>();
+        while (true){
+            list.add(new HeadTest());
+        }
+    }
+}
