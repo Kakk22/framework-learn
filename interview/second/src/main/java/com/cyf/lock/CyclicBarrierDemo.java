93132e6c6ae5ae7b83ccb4c8440d591660bb97d4
Kakk22 <1029839308@qq.com>
2021-01-23 16:37:40 +0800

    CyclicBarrier Semaphore demo

---
+++ interview/second/src/main/java/com/cyf/lock/CyclicBarrierDemo.java
@@ -0,0 +1,33 @@
+package com.cyf.lock;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * CyclicBarrier 循环屏障  当线程累加到指定值时 调用其线程任务
+ * @author 陈一锋
+ * @date 2021/1/23 16:33
+ **/
+public class CyclicBarrierDemo {
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () ->
+                System.out.println("召唤神龙")
+        );
+
+        for (int i = 0; i < 7; i++) {
+            final int temp = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("收集到第" + temp + "龙珠");
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
+    }
+}
