d8e1e5e31b40c238a6894d8bd16e75bebcb48261
Kakk22 <1029839308@qq.com>
2021-02-05 17:17:56 +0800

    init im project

---
+++ im/src/main/java/com/cyf/im/netty/server/IMServerHandler.java
@@ -0,0 +1,16 @@
+package com.cyf.im.netty.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author 陈一锋
+ * @date 2021/2/5 15:35
+ **/
+public class IMServerHandler extends SimpleChannelInboundHandler {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+}
