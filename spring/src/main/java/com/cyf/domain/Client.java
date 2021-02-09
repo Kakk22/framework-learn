5fbc00057cc5d92840bcedacfb852c808a9a52a0
Kakk22 <1029839308@qq.com>
2021-02-09 23:14:17 +0800

    spring 解决循环依赖

---
+++ spring/src/main/java/com/cyf/domain/Client.java
@@ -0,0 +1,14 @@
+package com.cyf.domain;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author 陈一锋
+ * @date 2021/2/9 22:02
+ **/
+public class Client {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application.xml");
+        context.getBean("a");
+    }
+}
