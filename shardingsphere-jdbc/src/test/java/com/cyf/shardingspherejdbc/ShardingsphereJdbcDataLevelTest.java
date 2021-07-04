026cb1f2ddc9d9f951c097931194cf17e6116d03
Kakk22 <1029839308@qq.com>
2021-07-04 15:10:45 +0800

    sharding 水平分表

--- shardingsphere-jdbc/src/test/java/com/cyf/shardingspherejdbc/ShardingsphereJdbcDataLevelTest.java
+++ shardingsphere-jdbc/src/test/java/com/cyf/shardingspherejdbc/ShardingsphereJdbcDataLevelTest.java
@@ -7,26 +7,40 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+/**
+ * @author 陈一锋
+ * @date 2021/6/27 13:47
+ **/
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class ShardingsphereJdbcApplicationTests {
-
+@ActiveProfiles("data-level")
+public class ShardingsphereJdbcDataLevelTest {
     @Autowired
     private CourseMapper courseMapper;
-
-
-
-    //-------------------水平分表
+    //------------------水平分库
     @Test
-    void testAdd() {
+    void testDB1() {
         for (int i = 0; i < 10; i++) {
             Course course = new Course();
-            course.setCname("java" + i);
+            course.setCname("java");
             course.setCstatus("up");
+            //根据userid分库
             course.setUserId(100L);
+            courseMapper.insert(course);
+        }
+    }
+
+    @Test
+    void testDB2() {
+        for (int i = 0; i < 10; i++) {
+            Course course = new Course();
+            course.setCname("java");
+            course.setCstatus("up");
+            //根据userid分库
+            course.setUserId(101L);
             courseMapper.insert(course);
         }
     }
@@ -35,9 +49,8 @@ class ShardingsphereJdbcApplicationTests {
     @Test
     void findOne() {
         QueryWrapper<Course> wrapper = new QueryWrapper<>();
-        wrapper.eq("cid", 615866324370849793L);
+        wrapper.eq("cid", 615908396490555392L);
         Course course = courseMapper.selectOne(wrapper);
         System.out.println(course);
     }
-
 }
