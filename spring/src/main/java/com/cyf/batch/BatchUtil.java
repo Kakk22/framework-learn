a1642fab589601935aab780620b83999befa354a
cyf <1029839308@qq.com>
2021-09-30 22:13:11 +0800

    mybatis批量提交工具修改为try-with-resource

--- spring/src/main/java/com/cyf/batch/BatchUtil.java
+++ spring/src/main/java/com/cyf/batch/BatchUtil.java
@@ -1,17 +1,10 @@
 package com.cyf.batch;
 
-import cn.hutool.core.collection.ListUtil;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.function.BiConsumer;
 
 /**
@@ -35,18 +28,20 @@ public class BatchUtil {
 
     public static <T, K> void batchCount(int maxCount, Collection<K> data, Class<T> mapperClazz, BiConsumer<T, K> consumer) {
         SqlSessionFactory sqlSessionFactory = SpringFactory.getBean(SqlSessionFactory.class);
-        SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
-        T mapper = sqlSession.getMapper(mapperClazz);
-        int i = 0;
-        for (K d : data) {
-            consumer.accept(mapper, d);
-            if ((++i) % maxCount == 0) {
-                sqlSession.flushStatements();
-                i = 0;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH)) {
+            T mapper = sqlSession.getMapper(mapperClazz);
+            int i = 0;
+            for (K d : data) {
+                consumer.accept(mapper, d);
+                if ((++i) % maxCount == 0) {
+                    sqlSession.commit();
+                    i = 0;
+                }
+            }
+            if (i != 0) {
+                sqlSession.commit();
             }
         }
-        sqlSession.flushStatements();
-        sqlSession.close();
     }
 
 
