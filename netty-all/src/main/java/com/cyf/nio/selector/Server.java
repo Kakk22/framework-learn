e1c6829e236a1f865636873d78948ee26d42d895
cyf <1029839308@qq.com>
2022-08-03 22:39:27 +0800

    feat(netty):selector key

--- netty-all/src/main/java/com/cyf/nio/selector/Server.java
+++ netty-all/src/main/java/com/cyf/nio/selector/Server.java
@@ -4,9 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
 /**
@@ -30,13 +34,27 @@ public class Server {
         ssc.bind(new InetSocketAddress(9091));
 
         while (true) {
-            //堵塞直至有事件发生
+            //堵塞直至有事件发生,如果有事件没被处理,则不被堵塞
+            //事件发生后,要么处理要么取消
             selector.select();
             final Iterator<SelectionKey> it = selector.selectedKeys().iterator();
-            while (it.hasNext()){
-                final ServerSocketChannel serverSocketChannel = (ServerSocketChannel) it.next().channel();
-                serverSocketChannel.accept();
-                log.info("连接成功:{}", serverSocketChannel);
+            while (it.hasNext()) {
+                final SelectionKey key = it.next();
+                if (key.isAcceptable()) {
+                    final ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                    final SocketChannel sc = serverSocketChannel.accept();
+                    log.info("连接成功:{}", sc);
+                    sc.configureBlocking(false);
+                    sc.register(selector, SelectionKey.OP_READ);
+                } else if (key.isReadable()) {
+                    final SocketChannel sc = (SocketChannel) key.channel();
+                    final ByteBuffer bf = ByteBuffer.allocate(30);
+                    sc.read(bf);
+                    bf.flip();
+                    log.info("接收到可读事件:{}", key);
+                    log.info("读取内容{}", StandardCharsets.UTF_8.decode(bf));
+                }
+                it.remove();
             }
 
         }
